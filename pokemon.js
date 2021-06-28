178176367926337af676023dee138c992880b590
abdul-malik <abdulmalikmohamed360@gmail.com>
2021-06-28 16:29:53 +0200

    added custom font

--- pokemon.js
+++ pokemon.js
@@ -1,25 +1,29 @@
 let base_URL = "https://pokeapi.co/api/v2/pokemon";
 
-function getPokemonList(url){
-    fetch(url)
-        .then(response => response.json())
-        .then(data =>{
-            console.log(data);
-            let pokemon =data.results;
-            let container = document.querySelector(".pokemon-list-container");
-            container.innerHTML = "";
-            pokemon.forEach((btn) => {
-                container.innerHTML += `<button onclick="getPokemonInfo('${btn.url}')">${btn.name}</button>`;
-            });
-            container.innerHTML += `<br><br><button onclick="getPokemonList('${data.next}')">Next</button>`;});
+function getPokemonList(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let pokemon = data.results;
+      let container = document.querySelector(".pokemon-list-container");
+      container.innerHTML = "";
+      pokemon.forEach((btn) => {
+        container.innerHTML += `<button class="pokebtn" onclick="getPokemonInfo('${btn.url}')">${btn.name}</button>`;
+      });
+      container.innerHTML += `<br><br><button onclick="getPokemonList('${data.previous}')">Previous</button>`;
+      container.innerHTML += `<button onclick="getPokemonList('${data.next}')">Next</button>`;
+    });
 }
 getPokemonList(base_URL);
 
-function getPokemonInfo(url){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        document.querySelector(".pokemon-info").innerHTML = `<img src= "${ data.sprites.front_default}">`
-    })
+function getPokemonInfo(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector(
+        ".pokemon-info"
+      ).innerHTML = `<img src= "${data.sprites.front_default}">`;
+    });
 }
